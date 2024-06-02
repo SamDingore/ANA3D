573d3d43db8afb9c0f9d6e886f5434787fe52b18
SamDingore <sameerdingore.iam@gmail.com>
2024-06-02 00:53:08 -0700

    added smart loader

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,25 +1,43 @@
 // pages/index.tsx
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import dynamic from 'next/dynamic';
+import { Button, Input } from '@mantine/core';
 
 const DynamicMoleculeViewer = dynamic(() => import('../components/MoleculeViewer'), { ssr: false });
 
 const Home: React.FC = () => {
+  const [pdbId, setPdbId] = useState<string>('');
   const [pdbData, setPdbData] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
-  useEffect(() => {
-    fetch('/1Y26.pdb')
+  const handleFetchPdbData = () => {
+    setLoading(true);
+    fetch(`https://files.rcsb.org/download/${pdbId.toUpperCase()}.pdb`)
       .then(response => response.text())
-      .then(data => setPdbData(data));
-  }, []);
-
-  if (!pdbData) {
-    return <div>Loading...</div>;
-  }
+      .then(data => {
+        setPdbData(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching PDB data:', error);
+        setLoading(false);
+      });
+  };
 
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <DynamicMoleculeViewer pdbData={pdbData} />
+    <div style={{ width: '100vw', height: '100vh', position:'fixed', alignItems: 'center', justifyContent: 'center' }}>
+      <div style={{ marginBottom: 20 }}>
+        <Input
+          placeholder="Enter PDB ID"
+          value={pdbId}
+          onChange={(event) => setPdbId(event.currentTarget.value)}
+          style={{ marginRight: 10 }}
+        />
+        <Button onClick={handleFetchPdbData} disabled={!pdbId || loading}>
+          {loading ? 'Loading...' : 'Load Structure'}
+        </Button>
+      </div>
+      {pdbData && <DynamicMoleculeViewer pdbData={pdbData} />}
     </div>
   );
 };
